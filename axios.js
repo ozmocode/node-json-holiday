24d88b3ba498da80c8d73a6fc5cf34520fb17755
Ozy Aloysius <12198383+ozmocode@users.noreply.github.com>
2020-12-13 14:35:10 +0700

    update axios

--- axios.js
+++ axios.js
@@ -1,9 +1,18 @@
 const axios = require('axios')
+const http = require('http')
 
-async function fetchData() {
-    let res = await axios.get('https://cdn.jsdelivr.net/gh/niyoko/libur-nasional/data/2020.json')
-    let data = res.data
-    console.log(data)
-}
-
-fetchData()
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        console.log('hit')
+        let data = []
+        async function fetchData() {
+            let res = await axios.get('https://cdn.jsdelivr.net/gh/niyoko/libur-nasional/data/2020.json')
+            data = res.data
+            console.log(data)
+        }
+        fetchData()
+        res.write(JSON.stringify(data))
+        res.end()
+    }
+})
+server.listen(5000)
